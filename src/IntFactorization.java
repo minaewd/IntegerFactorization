8e12e5df223d34b6dbe32ad04de6d8101452d520
Дмитрий <minaewdmitriy46@gmail.com>
2018-08-05 01:22:59 +0300

    Исправлен метод поиска простых чисел (решето Эратосфена). Добавлен метод факторизации введенного числа путем подбора делителей

--- src/IntFactorization.java
+++ src/IntFactorization.java
@@ -1,6 +1,6 @@
 public class IntFactorization {
     public static void main(String[] args) {
-        sieveOfEratosthenes(100);
+        trialDivision(65);
     }
 
     /*
@@ -9,12 +9,31 @@ public class IntFactorization {
      */
     private static int[] sieveOfEratosthenes(int factorNumber) {
         int[] tempArray = new int[factorNumber];
-        for (int i=0; i<factorNumber; i++) tempArray[i] = 1;
+        for (int i=2; i<factorNumber; i++) tempArray[i] = 1;
         for (int k=2; k*k<factorNumber; k++){
             if (tempArray[k] == 1) {
                 for (int j = k * k; j<factorNumber; j += k) tempArray[j] = 0;
             }
         }
         return tempArray;
+    }
+
+    /*
+    Метод позволяет факторизовать введенное число на простые множители методом подбора делителей
+     */
+    private static StringBuffer trialDivision(int factorNumber) {
+        int[] primes = sieveOfEratosthenes(factorNumber);
+        String result = "";
+        for (int i=2; i<primes.length; i++) {
+            if (primes[i] == 1) {
+                while ((factorNumber % i == 0) & (factorNumber != 0)) {
+                    factorNumber = factorNumber / i;
+                    result += Integer.toString(i) + " * ";
+                }
+            }
+        }
+        StringBuffer str = new StringBuffer(result);
+        str.delete(str.length()-3, str.length());
+        return str;
     }
 }
